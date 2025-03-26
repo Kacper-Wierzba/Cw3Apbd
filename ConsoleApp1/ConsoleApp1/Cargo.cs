da6b99c0b8b146f77d9de185c43579eae17c74df
Kacper-Wierzba <s30508@pjwstk.edu.pl>
2025-03-26 09:59:01 +0100

    created Kontener and support classes

---
+++ ConsoleApp1/ConsoleApp1/Cargo.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1;
+
+public class Cargo
+{
+    public string Type { get; set; }
+    public float Mass { get; set; }
+
+    public bool Hazardous { get; set; }
+}
---
+++ ConsoleApp1/ConsoleApp1/Kontener.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp1;
+
+
+public abstract class Kontener
+{
+    public float MassWithCargo{set; get;}
+    public float MassOfContainer{set; get;}
+    public float Height{set; get;}
+    public float Depth{set; get;}
+    public float MaxVolumeInKg{set; get;}
+
+    public Char Type { get; }
+    
+    public String SerialNumber { get; }
+
+    protected Kontener(char type)
+    {
+        Type = type;
+        SerialNumber = CreateSerialNumber();
+    }
+    
+    private string CreateSerialNumber()
+    {
+        return ("KON-"+Type+"-"+Kontener.nextNum);
+    }
+    
+    private static int nextNum
+    {
+        set
+        {
+            throw new Exception("cant change automated filed");
+        }
+        get
+        {
+            return nextNum++;
+        }
+    }
+    
+    abstract public void Empty();
+    abstract protected void Zaladunek(Cargo cargo);
+
+    private void DontOverflow(float Mass)
+    {
+        if(Mass>MaxVolumeInKg)
+            throw new OverfillException("Maximum volume is " + MaxVolumeInKg);
+    }
+    
+    public void Fill(Cargo cargo)
+    {
+        DontOverflow(cargo.Mass);
+        Zaladunek(cargo);
+    }
+    
+}
