28ccd96993b005b4c20ac4103c79855f3feb0c7f
Kacper-Wierzba <s30508@pjwstk.edu.pl>
2025-03-26 10:01:35 +0100

    created 1st type of Kontener and support interface

---
+++ ConsoleApp1/ConsoleApp1/KontenerPlyn.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+
+
+public class KontenerPlyn : Kontener
+{
+    public KontenerPlyn(float massOfContainer, float height, float depth, float maxVolumeInKg) : base('L')
+    {
+        MassOfContainer = massOfContainer;
+        Height = height;
+        Depth = depth;
+        MaxVolumeInKg = maxVolumeInKg;
+    }
+    
+    public override void Empty()
+    {
+        throw new NotImplementedException();
+    }
+    
+    protected override void Zaladunek(Cargo cargo)
+    {
+        MassWithCargo += cargo.Mass;
+    }
+}
